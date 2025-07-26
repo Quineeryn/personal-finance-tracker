d575f53c919a5954bdd1dbc3f7e22218acbe49b9
Quineeryn <canandraeka10@gmail.com>
2025-07-26 23:58:29 +0700

    chore: configure API URL to use env variables

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/v1/auth'; // URL Backend kita
+const API_URL = 'import.meta.env.VITE_API_URL'; // URL Backend kita
 
 const login = (email, password) => {
   return axios.post(`${API_URL}/login`, {

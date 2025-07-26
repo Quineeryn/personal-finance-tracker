398dc7ce530a73e36eef7dcbc183f310e8a42b71
Quineeryn <canandraeka10@gmail.com>
2025-07-27 00:17:04 +0700

    fix: configure CORS for prod

--- frontend/src/api/transactionService.js
+++ frontend/src/api/transactionService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/v1/transactions';
+const API_URL = 'import.meta.env.VITE_API_URL';
 
 const getAuthHeaders = (token) => {
   return { headers: { 'x-access-token': token } };

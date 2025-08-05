68b8438624fa0a24b64d6f6b5d1c2a90e68d1ce1
Quineeryn <canandraeka10@gmail.com>
2025-08-05 11:42:58 +0700

    fix: fix improt path

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import AuthService from '../api/auth.service';
+import AuthService from '../api/authService';
 
 // Import komponen Shadcn/UI
 import { Button } from '@/components/ui/button';
